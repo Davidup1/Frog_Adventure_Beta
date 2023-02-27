ef934b52569cfd09301273a83c0367ad8c885c54
Davidup1 <liyujie2003@bupt.edu.cn>
2023-02-27 20:35:03 +0800

    修改了仓库位置

---
+++ props/advancedDice.py
@@ -0,0 +1,6 @@
+from dice import Dice
+
+
+class AdvancedDice(Dice):
+    def __init__(self):
+        super(AdvancedDice,self).__init__()
---
+++ props/magicDice.py
@@ -0,0 +1,5 @@
+from dice import Dice
+
+class MagicDice(Dice):
+    def __init__(self):
+        super(MagicDice,self).__init__()
