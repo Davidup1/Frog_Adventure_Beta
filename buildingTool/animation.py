93b3646da5a638bbfb790dc593ffe9d033c80b8f
DeltaTUI <DeltaTUI@outlook.com>
2023-03-02 20:49:22 +0800

    update

---
+++ buildingTool/animation.py
@@ -0,0 +1,4 @@
+class Animation():
+    def __init__(self):
+        self.frames = []
+        self.interval = 10  # 单位ms(暂定)
--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from buildingTool.gameCirculation import gameCirculation
 
 class Game():
     def __init__(self):
-        filename = './image/sky.jpg'
+        filename = './image/bg_f1.png'
         pygame.init()
         pygame.display.set_caption("蛙蛙勇闯地牢")
-        screen = pygame.display.set_mode((800,800))
-        imageLoading(screen,filename,(0,0))
+        screen = pygame.display.set_mode((960, 539))
+        imageLoading(screen, filename, (0, 0))
 
         while True:
             # 游戏内部循环中的事件处理
             gameCirculation(self)
             pygame.display.update()
+
 
 if __name__ == "__main__":
     game = Game()
