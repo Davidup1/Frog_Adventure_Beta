6626a944ca72657d83766bcb8e22c8a4a934f75d
Davidup1 <liyujie2003@bupt.edu.cn>
2023-03-25 10:03:37 +0800

    稍微改动了以下table类onMouseHover方法

--- buildingTool/info.py
+++ buildingTool/info.py
@@ -1,13 +1,13 @@
 def show_info(game):
     fps_text = game.font.render(str(int(game.clock.get_fps())), False, (255,255,255))
-    game.screen.blit(fps_text, (9, 6))
+    game.screen.blit(fps_text, (9, 0))
 
     game.game_frame_cnt += 1
     cnt_text = game.font.render(str(game.game_frame_cnt), False, (255, 255, 255))
-    game.screen.blit(cnt_text, (9, 26))
+    game.screen.blit(cnt_text, (9, 20))
 
     mouse_pos_text = game.font.render("%d,%d" % game.mouse.rect.topleft, False, (255, 255, 255))
-    game.screen.blit(mouse_pos_text, (36, 6))
+    game.screen.blit(mouse_pos_text, (36, 0))
 
     game_floor = game.font.render("floor %d" % game.cur_level, False, (255, 255, 255))
-    game.screen.blit(game_floor, (860, 20))
+    game.screen.blit(game_floor, (880, 14))
